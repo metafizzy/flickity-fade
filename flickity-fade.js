03b9a17806d1879fae004a79d89500ae12207274
David DeSandro <desandrocodes@gmail.com>
2019-01-12 21:56:59 -0500

    🌅 initial commit with working prototype

---
+++ flickity-fade.js
@@ -0,0 +1,88 @@
+/* jshint browser: true, undef: true, unused: true */
+/* globals Flickity */
+
+var Cell = Flickity.Cell;
+
+Cell.prototype.setPosition = function( x ) {
+  this.x = x;
+  this.updateTarget();
+  // use target position for fade
+  if ( this.parent.options.fade ) {
+    // fade posiiton
+    var isOriginLeft = this.parent.originSide == 'left';
+    var marginProperty = isOriginLeft ? 'marginLeft' : 'marginRight';
+    x = this.size[ marginProperty ] + this.size.width * this.parent.cellAlign;
+    x *= -1;
+  }
+  this.renderPosition( x );
+};
+
+var proto = Flickity.prototype;
+
+// var updateSlides = proto.updateSlides;
+// proto.updateSlides = function() {
+//   updateSlides.apply( this, arguments );
+//   this.slideDistances
+//   if ( !this.options.fade ) {
+//     return;
+//   }
+// };
+
+var positionSlider = proto.positionSlider;
+Flickity.prototype.positionSlider = function() {
+  if ( !this.options.fade ) {
+    positionSlider.apply( this, arguments );
+    return;
+  }
+  // position fade slider
+  var x = this.cursorPosition;
+  // reverse if right-to-left and using transform
+  x = this.options.rightToLeft ? -x : x;
+  var value = this.getPositionValue( x );
+  this.slider.style.transform = 'translateX(' + value + ')';
+
+  this.fadeSlides();
+
+  // scroll event
+  var firstSlide = this.slides[0];
+  if ( firstSlide ) {
+    var positionX = -this.x - firstSlide.target;
+    var progress = positionX / this.slidesWidth;
+    this.dispatchEvent( 'scroll', null, [ progress, positionX ] );
+  }
+};
+
+proto.fadeSlides = function() {
+  if ( this.slides.length < 2 ) {
+    return;
+  }
+
+  // calculate closest previous slide
+  var prevIndex = 0;
+  for ( var i=0; i < this.slides.length; i++ ) {
+    var slide = this.slides[i];
+    if ( -this.x < slide.target ) {
+      break;
+    }
+    prevIndex = i;
+  }
+
+  var prevSlide = this.slides[prevIndex];
+  var nextSlide = this.slides[ prevIndex + 1 ];
+  var distance = nextSlide.target - prevSlide.target;
+
+  var incProgress = ( -this.x - prevSlide.target ) / distance;
+  // console.log( incProgress );
+  this.slides.forEach( function( slide, i ) {
+    var alpha = 0;
+    if ( i == prevIndex ) {
+      // previous slide
+      alpha = 1 - incProgress;
+    } else if ( i == prevIndex + 1 ) {
+      alpha = incProgress;
+    }
+    slide.cells.forEach( function( cell ) {
+      cell.element.style.opacity = alpha;
+    });
+  });
+};
